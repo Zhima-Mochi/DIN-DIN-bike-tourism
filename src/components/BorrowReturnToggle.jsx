cdb810f459b9f19ef246073a6dc5acf83d8cdb36
Zhima-Mochi <yehgoter@gmail.com>
2021-11-22 13:20:23 +0800

    fix icon css content

--- src/components/BorrowReturnToggle.jsx
+++ src/components/BorrowReturnToggle.jsx
@@ -1,8 +1,11 @@
-export default function BorrowReturnToggle({isToggleOn,handleClick}){
+import bike from '../assets/icon/bike.svg'
+import bike_orange from '../assets/icon/bike_orange.svg'
+
+export default function BorrowReturnToggle({ isToggleOn, handleClick }) {
     return (
         <div className={"bike-toggle select-none transition-all flex "} onClick={handleClick}>
-            <img className={'bike-toggle-icon  flex-none '+(isToggleOn?'return':'borrow')}/>
-            <span className={`bike-toggle-word  m-auto `+(isToggleOn?'return':'borrow')}>{isToggleOn?"歸還模式":"租借模式"}</span>
+            <img className={'bike-toggle-icon  flex-none ' + (isToggleOn ? 'return' : 'borrow')} src={isToggleOn ? bike_orange : bike} />
+            <span className={`bike-toggle-word  m-auto ` + (isToggleOn ? 'return' : 'borrow')}>{isToggleOn ? "歸還模式" : "租借模式"}</span>
         </div>
     )
 }
